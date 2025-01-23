9b9ec899d99375d386fc5edcb2ee2e6c46cff47d
OwaisPaul <152506988+OwaisPaul@users.noreply.github.com>
2025-01-23 11:50:18 +0530

    Creating the grid

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 
 const container = document.getElementById('container');
-
-
 const button = document.getElementById('btn');
 
 button.addEventListener('click', ()=>{
+    // clearing the previous grid
+        container.innerText = '';
     const input = prompt('enter the size')
+  const size = parseInt(input)
+    //  check if the input is a valid number
+  if (isNaN(size) || size >= 100){
+    alert('invalid input')
+    return;
+}
+
+// creating grid
+
+ for(let i=0; i < size * size; i++){
+
+        const box = document.createElement('div');
+       
+        box.classList.add('item');
+        box.style.backgroundColor = 'purple';
+        // box.style.width = `${100 / size}%`; // width based on grid size
+        // box.style.height = `${100 / size}%`; // height based on grid size
+
+        // Changing the color on mouseover
+
+        box.addEventListener('mouseover', ()=>{
+            box.style.background = 'orange';
+        });
+        container.appendChild(box);
+    }
+        container.style.display = 'grid';
+        container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    
 });
-
-
-const box = document.createElement('div');
-box.addEventListener('mouseover', ()=>{
-    box.style.background = 'orange';
-})
-box.classList.add('item')
-box.style.backgroundColor = 'purple';
-
-container.appendChild(box);
